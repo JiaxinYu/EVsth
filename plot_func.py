83d8d5e30cc6145458960fa85b3c269d03eeee60
Jiaxin Yu <9447954+JiaxinYu@users.noreply.github.com>
2021-01-20 15:08:32 +0800

    Update plot_func.py
    adding moving avg

--- plot_func.py
+++ plot_func.py
@@ -20,26 +20,41 @@ def getshiftwork(df, date):
   return [t0, t1, t2, t3]
 
 
-def plot_alltime(df):
-  # plot 3 days
+def cal_ma(seq):
+  wsize = 3
+
+  n_series = pd.Series(seq)
+  windows = n_series.rolling(wsize)
+  ma = windows.mean()
+
+  ma_list = ma.tolist()
+  return ma_list[wsize - 1:]
+
+
+def plot_index(df, date, idxname, cmap='b'):
+  # plot with 3 shifts
   fig, axes = plt.subplots(3, 1, figsize=(28, 12), dpi=300)
-  axes[0].set_title('HPI', fontsize=20)
-  axes[1].set_title('Eadyn', fontsize=20)
-  axes[2].set_title('CO', fontsize=20)
-
-  time0 = df['time'][0] + '  ' + df['date'][0]
-  timet = df['time'][len(df)-1] + '  ' + df['date'][len(df)-1]
-
-  im = axes[0].plot(df['HPI'].astype('int').to_numpy(), label='HPI', color='b')
-  plt.sca(axes[0])
-  plt.xticks(fontsize=16)
-  plt.xticks([0, len(df1)], [time0, timet])
+  swt = getshiftwork(df, date)
+  axes[0].set_title(date+'_'+idxname, fontsize=20)
   
-  im = axes[1].plot(df['Eadyn'].astype('float').to_numpy(), label='Eadyn', color='r', alpha=0.6)
-  im = axes[1].plot(np.ones(len(df)) - 0.1, color='r', linestyle='--', lw=3)
-  
-  im = axes[2].plot(df['CO'].astype('float').to_numpy(), label='CO', color='g')
-  plt.savefig(rootpath/'index_{}.png'.format(dt.date.today()), dpi=300)
+  if idxname == 'BP':
+    for i in range(3):
+      # im = axes[i].plot(df['SBP'][swt[i]:swt[i+1]].astype('float').to_numpy(), color='b')
+      # im = axes[i].plot(df['DBP'][swt[i]:swt[i+1]].astype('float').to_numpy(), color='g')
+      im = axes[i].plot(cal_ma(df['SBP'][swt[i]:swt[i+1]].astype('float')), color='b')
+      im = axes[i].plot(cal_ma(df['DBP'][swt[i]:swt[i+1]].astype('float')), color='g')
+      plt.sca(axes[i])
+      plt.xticks(fontsize=14)
+      plt.xticks(np.array(range(0, 180*8, 180)), np.array(range(i*8, (i+1)*8)))
+  else:
+    for i in range(3):
+      # im = axes[i].plot(df[idxname][swt[i]:swt[i+1]].astype('float').to_numpy(), color=cmap)
+      im = axes[i].plot(cal_ma(df[idxname][swt[i]:swt[i+1]].astype('float')), color=cmap)
+      plt.sca(axes[i])
+      plt.xticks(fontsize=14)
+      plt.xticks(np.array(range(0, 180*8, 180)), np.array(range(i*8, (i+1)*8)))
+
+  plt.savefig(rootpath/'{}_{}.png'.format(idxname, date), dpi=300)
 
 
 def plot_index(df, date, idxname, cmap='b'):

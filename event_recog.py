11aafd0fec0d91c95b827a49e2849d19e71eb9bc
Jiaxin Yu <9447954+JiaxinYu@users.noreply.github.com>
2021-02-08 17:18:21 +0800

    Update event_recog.py
    get trend of pat's situations
    1. added get_trend function to get increased or decreased of indexes
    2. added pattern_recon to identified the causes of cv problems

--- event_recog.py
+++ event_recog.py
@@ -2,22 +2,36 @@ def pulse_pressure(SBP, DBP):
   return (SBP - DBP)
 
 
-def pattern_recon(df):
-  ## 1. CO down: CO, dPdt, PP down; HPI up
-  ## 2. CO up: CO, dPdt, PP up; HPI down
-  ## 3. vasoconstriction: SVR, Eadyn up; HPI down
+def get_trend(df, date, idxname, t0, t1):
+  if idxname == 'PP':
+    var = df['SBP'][t0:t1].astype('float') - df['DBP'][t0:t1].astype('float')
+  else:
+    var = df[idxname][t0:t1].astype('float')
+  
+  trend_var = var - cal_ma(var)
+  trend_var[trend_var > 0] = 1
+  trend_var[trend_var < 0] = -1
+  return trend_var
+
+
+def pattern_recon(df, date, t0, t1):
+  df_hpi = pd.DataFrame(get_trend(df, date, 'HPI', t0, t1), columns=['HPI'])
+  for i in ['CO', 'dPdt', 'PP', 'SVR', 'Eadyn']:
+    df_temp = pd.DataFrame(get_trend(df, date, i, t0, t1), columns=[i])
+    df_hpi = df_hpi.join(df_temp)
 
   event_type = []
-  if HPI[i] > HPI_ma[i]:
-    if all(CO[i] > CO_ma[i], dPdt[i] > dPdt_ma[i], PP[i] > PP_ma[i]):
-      event_type.append(1)
+  for i in df_hpi.index:
+    if df_hpi['HPI'][i] > 0:
+      if all([(df_hpi['CO'][i] > 0), (df_hpi['dPdt'][i] > 0), (df_hpi['PP'][i] > 0)]):
+        event_type.append(1) # CO reduced: CO, dPdt and PP reduced; HPI increased
+      else:
+        event_type.append(0)
+    elif df_hpi['HPI'][i] < 0:
+      if all([(df_hpi['CO'][i] < 0), (df_hpi['dPdt'][i] < 0), (df_hpi['PP'][i] < 0)]):
+        event_type.append(2) # CO increased: CO, dPdt and PP increased; HPI reduced
+      elif all([(df_hpi['SVR'][i] > 0), (df_hpi['Eadyn'][i] > 0)]):
+        event_type.append(3) # vasoconstriction: SVR and Eadyn increased; HPI reduced
     else:
-      event_type.append(0)
-  elif HPI[i] < HPI_ma[i]:
-    if all(CO[i] < CO_ma[i], dPdt[i] < dPdt_ma[i], PP[i] < PP_ma[i]):
-      event_type.append(2)
-    elif all(SVR[i] > SVR_ma[i], Eadyn[i] > Eadyn_ma[i]):
-      vent_type.append(3)
-    else:
-      event_type.append(0)
+      event_type.append(0) # others
   return event_type
